1ad617cee448aca8398d8c88de336cf949a759f0
minwoojeon <jeonminwoo0625@gmail.com>
2023-10-03 23:58:30 +0900

    Entity Class 개발중

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Getter
+@Setter
+@Embeddable
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity
+@DiscriminatorValue("M")
+public class Movie extends Item {
+
+    private String director;
+    private String actor;
+
+}
